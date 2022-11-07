c8952c4434b80df88002f66b1d48ea0164aae4a1
romee <romyoh@gmail.com>
2022-11-07 16:39:07 +0900

    theme update

--- Wisejhtml/Page1.cs
+++ Wisejhtml/Page1.cs
@@ -10,5 +10,18 @@ namespace Wisejhtml
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void htmlPanel1_ElementClick(object sender, HtmlPanelElementClickArgs e)
+        {
+			string a1 = e.Element.ToString();
+
+			AlertBox.Show(a1);
+
+			//if (a1 == "대기")
+			{ 
+				Page2 p2 = new Page2();
+				p2.Show();
+			}
+        }
+    }
 }
--- Wisejhtml/Page2.Designer.cs
+++ Wisejhtml/Page2.Designer.cs
@@ -28,35 +28,58 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.htmlPanel1 = new Wisej.Web.HtmlPanel();
+            this.splitContainer1 = new Wisej.Web.SplitContainer();
+            this.splitContainer2 = new Wisej.Web.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // htmlPanel1
+            // splitContainer1
             // 
-            this.htmlPanel1.Dock = Wisej.Web.DockStyle.Fill;
-            this.htmlPanel1.Focusable = false;
-            this.htmlPanel1.HtmlSource = "html\\B001.html";
-            this.htmlPanel1.Location = new System.Drawing.Point(0, 0);
-            this.htmlPanel1.Name = "htmlPanel1";
-            this.htmlPanel1.Size = new System.Drawing.Size(647, 507);
-            this.htmlPanel1.TabIndex = 0;
-            this.htmlPanel1.TabStop = false;
+            this.splitContainer1.Location = new System.Drawing.Point(43, 109);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(433, 335);
+            this.splitContainer1.SplitterDistance = 218;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = Wisej.Web.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = Wisej.Web.Orientation.Horizontal;
+            this.splitContainer2.Size = new System.Drawing.Size(209, 335);
+            this.splitContainer2.SplitterDistance = 165;
+            this.splitContainer2.TabIndex = 0;
             // 
             // Page2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
-            this.Controls.Add(this.htmlPanel1);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Page2";
             this.Size = new System.Drawing.Size(647, 507);
             this.Text = "Page1";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
 		}
 
         #endregion
 
-        private Wisej.Web.HtmlPanel htmlPanel1;
+        private Wisej.Web.SplitContainer splitContainer1;
+        private Wisej.Web.SplitContainer splitContainer2;
     }
 }
 
