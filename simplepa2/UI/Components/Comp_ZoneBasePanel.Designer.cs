66e214c9086b0b595ec28477f4a3b3845bbafb70
최영 <conculer@gmail.com>
2023-02-21 20:57:17 +0900

    Zone Base 파일 생성ㅇ

---
+++ simplepa2/UI/Components/Comp_ZoneBasePanel.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace simplepa2.UI.Components
+{
+    partial class Comp_ZoneBasePanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Comp_ZoneBasePanel
+            // 
+            this.Name = "Comp_ZoneBasePanel";
+            this.Size = new System.Drawing.Size(557, 569);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ simplepa2/UI/Components/Comp_ZoneBasePanel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Wisej.Web;
+
+namespace simplepa2.UI.Components
+{
+    public partial class Comp_ZoneBasePanel : Wisej.Web.UserControl
+    {
+        public Comp_ZoneBasePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
